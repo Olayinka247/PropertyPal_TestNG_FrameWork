2c76730d0b32b003e094764ec1c734c8e165e1ae
Olayinka Ogunbiyi <97066655+Olayinka247@users.noreply.github.com>
2023-12-20 15:58:20 +0000

    Test Case Scenarios for 2,3,5 beds

--- PropertyPalFramework/src/main/java/libary/Constants.java
+++ PropertyPalFramework/src/main/java/libary/Constants.java
@@ -12,8 +12,10 @@ public class Constants {
 	public static final String[] CURRENCY = {"£","€"};
 	public static final String[] MIN_BED = {"Any","1 Bed min","2 Beds min","3 Beds min","4 Beds min","5 Beds min","6 Beds min"};
 	public static final String[] MAX_BED = {"Any","1 Bed max","2 Beds max","3 Beds max","4 Beds max","5 Beds max","6 Beds max"};
-	public static final String[] MIN_AMOUNT = {"Any","€20,000 min","€30,000 min","€40,000 min","€50,000 min","€60,000 min","€2,000,000 min"};
-	public static final String[] MAX_AMOUNT = {"Any","€20,000 max","€30,000 max","€40,000 max","€50,000 max","€60,000 min","€2,000,000 max"};
+	public static final String[] MIN_AMOUNTE = {"Any","€20,000 min","€30,000 min","€40,000 min","€50,000 min","€60,000 min","€2,000,000 min"};
+	public static final String[] MAX_AMOUNTE = {"Any","€20,000 max","€30,000 max","€40,000 max","£50,000 max","€60,000 min","€2,000,000 max"};
+	public static final String[] MIN_AMOUNTP = {"Any","£20,000 min","£30,000 min","£40,000 min","£50,000 min","£60,000 min","£2,000,000 min"};
+	public static final String[] MAX_AMOUNTP = {"Any","£20,000 max","£30,000 max","£40,000 max","£50,000 max","£60,000 min","£2,000,000 max"};
 	public static final String[] PROPERTY_TYPE = {"Apartments","All Houses","Bungalows","Detached houses","Semi-detached houses","Terraced houses","Houses and Land"};
 	public static final String[] BED_TYPE = {"Any","1 Bed","2 Bed","3 Bed","4 BeD","5 Bed","6 Bed"};
 
--- PropertyPalFramework/src/test/java/end_2_end/Result_Page_e2e.java
+++ PropertyPalFramework/src/test/java/end_2_end/Result_Page_e2e.java
@@ -30,15 +30,41 @@ public class Result_Page_e2e {
 	}
 	
 	@Test
-	public void testcase_001() throws Exception {
+	public void CheckFor2Beds_Testcase_001() throws Exception {
 		result = new Result_Page(driver);
 		result.handleCookiePopup();
 		result.searchForAPostCode(Constants.POSTCODE);
-		result.currencyAndPriceRangeOption(Constants.CURRENCY[1],Constants.MIN_AMOUNT[1],Constants.MAX_AMOUNT[6]);
+		result.currencyAndPriceRangeOption(Constants.CURRENCY[1],Constants.MIN_AMOUNTE[1],Constants.MAX_AMOUNTE[6]);
 		result.selectABedroomFilter(Constants.MIN_BED[2], Constants.MAX_BED[2]);
 		result.propertyTypesOption(Constants.PROPERTY_TYPE[0]);
 		result.clickSearch();
 		result.verifyResults(Constants.BED_TYPE[2]);
+	
+	}
+	
+	@Test
+	public void CheckFor3Beds_Testcase_002() throws Exception {
+		result = new Result_Page(driver);
+		result.handleCookiePopup();
+		result.searchForAPostCode(Constants.POSTCODE);
+		result.currencyAndPriceRangeOption(Constants.CURRENCY[0],Constants.MIN_AMOUNTP[1],Constants.MAX_AMOUNTP[6]);
+		result.selectABedroomFilter(Constants.MIN_BED[3], Constants.MAX_BED[3]);
+		result.propertyTypesOption(Constants.PROPERTY_TYPE[1]);
+		result.clickSearch();
+		result.verifyResults(Constants.BED_TYPE[3]);
+	
+	}
+	
+	@Test
+	public void CheckFor5Beds_Testcase_003() throws Exception {
+		result = new Result_Page(driver);
+		result.handleCookiePopup();
+		result.searchForAPostCode(Constants.POSTCODE);
+		result.currencyAndPriceRangeOption(Constants.CURRENCY[1],Constants.MIN_AMOUNTE[1],Constants.MAX_AMOUNTE[6]);
+		result.selectABedroomFilter(Constants.MIN_BED[5], Constants.MAX_BED[5]);
+		result.propertyTypesOption(Constants.PROPERTY_TYPE[1]);
+		result.clickSearch();
+		result.verifyResults(Constants.BED_TYPE[5]);
 	
 	}
 }
