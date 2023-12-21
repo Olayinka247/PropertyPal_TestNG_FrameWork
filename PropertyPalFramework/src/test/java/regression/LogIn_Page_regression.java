aace8b571c0a35f5cd03618cb8a6b96e23c64ca5
Olayinka Ogunbiyi <97066655+Olayinka247@users.noreply.github.com>
2023-12-21 16:24:07 +0000

    Log In Regression

---
+++ PropertyPalFramework/src/test/java/regression/LogIn_Page_regression.java
@@ -0,0 +1,61 @@
+package regression;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.assertthat.selenium_shutterbug.core.Shutterbug;
+
+import libary.Constants;
+import pages.LogIn_Page;
+
+
+public class LogIn_Page_regression {
+	WebDriver driver;
+	LogIn_Page result;
+
+	
+	@BeforeMethod
+	public void setup() throws Exception {
+		driver = libary.BrowserLibary.launchBrowser("firefox");
+		driver.manage().window().maximize();
+		driver.get(Constants.URL);
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		result = new LogIn_Page(driver);
+		result.handleCookiePopup();
+	}
+	
+	@AfterMethod
+	public void teardown(ITestResult result) {
+		if(ITestResult.FAILURE == result.getStatus()) {
+			Shutterbug.shootPage(driver).withName(result.getName()).save();
+		}
+		driver.quit();
+	}
+	
+	@Test
+	public void ConfirmUserEmail_Testcase_001() throws Exception{
+		result = new LogIn_Page(driver);
+		result.checkEmail(Constants.EMAIL[0]);
+		result.checkEmailValue("mik@aol.com");
+	}
+	
+	@Test
+	public void ConfirmEmailTakesAphabet_Testcase_002() throws Exception{
+		result = new LogIn_Page(driver);
+		result.checkEmail(Constants.EMAIL[0]);
+		result.checkEmailValue(Constants.EMAIL[0]);
+	}
+	
+	@Test
+	public void ConfirmErrorMsgForInvalidData_Testcase_003() throws Exception {
+		result = new LogIn_Page(driver);
+		result.logIn(Constants.EMAIL[0], Constants.PASSWORD[0]);
+		result.checkLogInErrorMessage();
+	}
+	
+}
